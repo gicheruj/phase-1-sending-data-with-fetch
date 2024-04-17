6978ba1eb85493c572ed9bf35d5ba738c7d50b09
John <jgicheru325@gmail.com>
2024-04-17 11:27:15 +0300

    phase-1-sending-data-with=fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+           name: userName, 
+           email: userEmail, 
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        document.body.innerHTML += `<p>New ID: ${data.id}</p>`;
+    })
+    .catch(error => {
+
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+    });
+}
+
+submitData("John Doe", "john@example.com");
+
